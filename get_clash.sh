906ba95397a3e95d6e579a3f1636435669336490
hebe061103 <109753786@qq.com>
2023-09-12 14:16:10 +0800

    2023-09-12 14:16:09

--- get_clash.sh
+++ get_clash.sh
@@ -25,6 +25,7 @@ urllist=("https://ghproxy.com/https://raw.githubusercontent.com/openrunner/clash
 "https://ghproxy.com/https://raw.githubusercontent.com/ermaozi/get_subscribe/main/subscribe/clash.yml"
 "https://clashnode.com/wp-content/uploads/$date1/$date2/$date3.yaml"
 "$subdz&url=https://free.jingfu.cf/vmess/sub"
+"$subdz&url=https://ghproxy.com/https://raw.githubusercontent.com/Jsnzkpg/Jsnzkpg/Jsnzkpg/Jsnzkpg"
 )
 for i in ${urllist[@]}
     do
