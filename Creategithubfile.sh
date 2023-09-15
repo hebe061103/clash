acf1f8a4f12f06eb04d28ecabf3aef3f78eab92e
hebe061103 <109753786@qq.com>
2023-09-15 09:11:09 +0800

    2023-09-15 09:11:09

--- Creategithubfile.sh
+++ Creategithubfile.sh
@@ -31,9 +31,9 @@ echo "$date 更新!" > README.md
 git init
 git add ./
 git commit -m "$date"
-git remote set-url origin https://$GITHUBTOKEN@github.com/hebe061103/clash.git
+result0=`git remote set-url origin https://$GITHUBTOKEN@github.com/hebe061103/clash.git`
+echo "$result0" >> /tmp/result0.log
 result=`git push -u origin master`
-echo "$result" >> /tmp/githubpull.log
 if echo "$result" | grep -e "set up to track remote branch";then
 date=$(date "+%Y-%m-%d %H:%M:%S")
 echo --$date-- "------------------------同步到github成功-------------------------" |tee -a /tmp/clash_run_log.log
