3c8e368c38400b7f50476786ad0807468613c714
hebe061103 <109753786@qq.com>
2023-10-21 12:08:48 +0800

    2023-10-21 12:08:48

--- get_clash.sh
+++ get_clash.sh
@@ -23,7 +23,6 @@ day=$(date "+%d")
 merge=$(date "+%Y%m%d")
 #以下列表可插入能直接下载的配置文件网址
 urllist=(
-"https://sub.789.st/sub?target=clash&url=https://ghproxy.com/https://raw.githubusercontent.com/Pawdroid/Free-servers/main/sub"
 "https://sub.789.st/sub?target=clash&url=https://clashnode.com/wp-content/uploads/$year/$month/$merge.txt"
 )
 for i in ${urllist[@]}
