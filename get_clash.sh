faf2004ec1040984338660c39811a613e5052486
hebe061103 <109753786@qq.com>
2023-10-21 13:20:01 +0800

    2023-10-21 13:20:01

--- get_clash.sh
+++ get_clash.sh
@@ -26,7 +26,7 @@ merge=$(date "+%Y%m%d")
 #以下列表可插入能直接下载的配置文件网址
 urllist=(
 "https://sub.789.st/sub?target=clash&url=https://clashnode.com/wp-content/uploads/$year/$month/$merge.txt"
-"https://sub.789.st/sub?target=clash&url=https://rvorch.treze.cc/clash/proxies?c=HK,TW,US,JP,SG"
+"https://ghproxy.com/https://raw.githubusercontent.com/snakem982/proxypool/main/clash73544.yaml"
 )
 for i in ${urllist[@]}
     do
